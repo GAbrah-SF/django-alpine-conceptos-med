74889a52c599d8a85d3bf5c6f60bebba8eeb3c94
Gabriel Abraham <ga.sanzfons@gmail.com>
2023-11-23 16:50:35 -0600

    feat: add docker-compose.yaml file

--- conceptos/admin.py
+++ conceptos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Concepto
+
 
 # Register your models here.
+@admin.register(Concepto)
+class TablaConcepto(admin.ModelAdmin):
+    list_display = ('id', 'concepto', 'significado', 'unidad', 'materia')
--- materias/admin.py
+++ materias/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Materia
+from .models import Materia, Unidad
 
 
 # Register your models here.
 @admin.register(Materia)
 class TablaMateria(admin.ModelAdmin):
     list_display = ('id', 'materia', 'objetivo', 'bimestre')
+
+
+@admin.register(Unidad)
+class TablaUnidad(admin.ModelAdmin):
+    list_display = ('id', 'unidad', 'materia')
