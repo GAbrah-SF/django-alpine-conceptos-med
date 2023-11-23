c1608b7e8ee78e06e9526ddd1ee9a07f0901afa4
Gabriel Abraham <ga.sanzfons@gmail.com>
2023-11-23 01:28:03 -0600

    feat: add .dockerignore file

--- app/settings.py
+++ app/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config['CREDENTIALS']['NAME'],
-        'USER': config['CREDENTIALS']['USER'],
-        'PASSWORD': config['CREDENTIALS']['PASSWORD'],
-        'HOST': config['CREDENTIALS']['HOST'],
-        'PORT': config['CREDENTIALS']['PORT'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
